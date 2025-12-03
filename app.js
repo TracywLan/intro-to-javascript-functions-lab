e3cab289a38f06d7cfbff6041726b45797550699
Tracywlan <tracylan12@gmail.com>
2025-12-03 15:05:46 -0500

    added question 11 and 12

--- app.js
+++ app.js
@@ -159,9 +159,9 @@ Complete the exercise in the space below:
 */
 function convertTemperature(temperature, scale) {
     if (scale === 'C'){
-        return (temperature * 9/5) + 32 + "(Fahrenheit)";
+        return (temperature * 9/5) + 32 + " (Fahrenheit)";
     } else {
-        return (temperature - 32) * 5/9 + "(Celsius)";
+        return (temperature - 32) * 5/9 + " (Celsius)";
     }
 }
 
@@ -199,3 +199,77 @@ function basicCalculator(num1, num2, operation) {
 
 
 console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
+
+/*
+Exercise 10: calculateGrade()
+
+Define a function called calculateGrade. 
+It should take a numerical score and return the corresponding letter 
+grade (A, B, C, D, F). 
+
+For example, 90 and above yields an 'A', 80-89 is a 'B', 
+and 70-79 is a 'C', 60-69 is a 'D' and anything lower than a 60 is an 'F'.
+
+Example: calculateGrade(100) should return A.
+
+Complete the exercise in the space below:
+*/
+
+function calculateGrade(score) {
+    switch (true) {
+        case (score >= 90):
+            return 'A';
+        case (score >= 80):
+            return 'B';
+        case (score >= 70):
+            return 'C';
+        case (score >= 60):
+            return 'D';
+        default:
+            return 'F';
+    }
+}
+
+console.log('Exercise 10 Result:', calculateGrade(85));
+
+
+/*
+Exercise 11: createUsername()
+
+Define a function called createUsername. 
+It should take a first name and a last name and return a username. 
+
+The username should be a combination of the following:
+- The first three letters of the first name.
+- The first three letters of the last name.
+- The total character count of the first and last name combined.
+
+Example: createUsername('Samantha', 'Green') should return 'SamGre13'.
+
+Complete the exercise in the space below:
+*/
+
+function createUsername(firstName, lastName) {
+    const firstPart = firstName.slice(0, 3);
+    const lastPart = lastName.slice(0, 3);
+    const totalLength = firstName.length + lastName.length;
+    return `${firstPart}${lastPart}${totalLength}`;
+}
+
+console.log('Exercise 11 Result:', createUsername("Samantha", "Green"));
+
+
+/*
+Exercise 12: numArgs()
+
+Challenge yourself with numArgs. 
+This function should return the count of arguments passed to it when called.
+
+Complete the exercise in the space below:
+*/
+
+function numArgs(...args) { 
+    return args.length;
+}
+
+console.log('Exercise 12 Result:', numArgs(1, 2, 3, 4));
